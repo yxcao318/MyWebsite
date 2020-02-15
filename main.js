50223073183df5c7e3d8942bda5ae2c56bff1792
Yuxin Cao <55064994+kellycao@users.noreply.github.com>
2020-02-15 11:35:42 -0500

    1st

---
+++ main.js
@@ -0,0 +1,36 @@
+//responsive menu
+function Toggle(){
+  var header = document.getElementById("header");
+  header.classList.toggle('active');
+}
+
+//menu bar
+
+var menu = document.getElementsByClassName("menu");
+menu[0].onmouseover = function(){
+this.innerHTML = "<span></span><span></span><span></span><span></span>主页 / Home";
+}
+menu[0].onmouseout = function(){
+  this.innerHTML = "主页 / Home";
+}
+
+menu[1].onmouseover = function(){
+this.innerHTML = "<span></span><span></span><span></span><span></span>关于我 / About";
+}
+menu[1].onmouseout = function(){
+  this.innerHTML = "关于我 / About";
+}
+
+menu[2].onmouseover = function(){
+this.innerHTML = "<span></span><span></span><span></span><span></span>作品集 / Portfolio";
+}
+menu[2].onmouseout = function(){
+  this.innerHTML = "作品集 / Portfolio";
+}
+
+menu[3].onmouseover = function(){
+this.innerHTML = "<span></span><span></span><span></span><span></span>联系我 / Contact";
+}
+menu[3].onmouseout = function(){
+  this.innerHTML = "联系我 / Contact";
+}
